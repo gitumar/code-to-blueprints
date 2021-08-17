8ce1ccd3794db60bb7ca0daf94625a9f9f2102dd
Umar <umarb8@yahoo.com>
2021-08-17 15:36:53 -0500

    Add files via upload

---
+++ BookFiles/Strategy/C#_Code/BruteForceStrategy.cs
@@ -0,0 +1,8 @@
+using System;
+public class BruteForceStrategy : BattleShipStrategy {
+
+
+  public void maneuver() {
+   Console.WriteLine("We can take these ships head on");
+  }
+}
---
+++ BookFiles/Strategy/C#_Code/Strategy_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Strategy_Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A tiny frigate wants some trouble");
+            BattleShip battleShip = new BattleShip(new BruteForceStrategy());
+            battleShip.engage();
+            Console.WriteLine("Four tiny frigates want some trouble");
+            battleShip.alterManeuvers(new DivideConquerStrategy());
+            battleShip.engage();
+            Console.WriteLine("An aircraft carrier group wants some trouble");
+            battleShip.alterManeuvers(new RetreatStrategy());
+            battleShip.engage();
+        }
+    }
+}
